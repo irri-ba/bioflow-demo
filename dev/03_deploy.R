520ed43435e821d5aed2dc3eea1aa42707feaa5d
irri-ba <irribreedinganalytics@gmail.com>
2025-03-12 13:06:02 +0800

    Update 03_deploy.R

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -65,11 +65,5 @@ rsconnect::setAccountInfo(name='',
 
 
 rsconnect::deployApp(account = "cgiar-market-intelligence")
-
-rsconnect::setAccountInfo(name='cgiar-market-intelligence',
-                          token='DB3778031938A406DDB9EA47006FB47D',
-                          secret='OYKpvd/zQUgIXhlzhMau9nJ/OhYeR3oiv0odKDCD')
-rsconnect::deployApp(account = "cgiar-market-intelligence", appName = "bioflow-demo")
-
 #
 # rsconnect::deployApp(account = "giovanny-covarrubias")
